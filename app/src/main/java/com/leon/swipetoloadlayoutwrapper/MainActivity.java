86d6cc8df021e537ca77418e6d7bbbe0c3253595
njleonzhang <scream3616@sina.com>
2017-06-30 10:02:23 +0800

    sample to use View as params of setFooterView

--- app/src/main/java/com/leon/swipetoloadlayoutwrapper/MainActivity.java
+++ app/src/main/java/com/leon/swipetoloadlayoutwrapper/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.leon.swipeToLoadLayout.SwipeToLoadLayoutWrapper;
 
@@ -48,7 +49,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        LoadMoreFooterView view = new LoadMoreFooterView(this);
+        // important
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        view.setLayoutParams(params);
+
         mWrapper.setHeaderView(SwipeToLoadLayoutWrapper.TYPE_GOOGLE)
-            .setFooterView(SwipeToLoadLayoutWrapper.TYPE_CLASSIC);
+//            .setFooterView(SwipeToLoadLayoutWrapper.TYPE_CLASSIC)
+            .setFooterView(view);
     }
 }
